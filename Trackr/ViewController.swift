57aefff824f839372ead96899ad6ad4e73e49288
Jack Wu <masterjack1997@gmail.com>
2020-04-09 10:27:34 -0400

    Initial Commit

---
+++ Trackr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trackr
+//
+//  Created by Jack on 4/9/20.
+//  Copyright © 2020 Jack Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
